d2043cb5e605a34c4a076813683873d5544ebb9b
Mirko Wawrowsky <mirko@wawrowsky.de>
2020-05-26 21:12:19 +0200

    routing and home component introduced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TeachingPhaseComponent } from './teaching-phase/teaching-phase.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'teach', component: TeachingPhaseComponent },
+
+  // authentication demo
+  // { path: 'login', component: LoginComponent }, {
+  //   path: 'protected',
+  //   component: ProtectedComponent, canActivate: [LoggedInGuard]
+  // },
+
+  // nested
+  // {
+  //   path: 'products',
+  //   component: ProductsComponent,
+  //   children: childRoutes
+  // }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeachingPhaseComponent
+    TeachingPhaseComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
